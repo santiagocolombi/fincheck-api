4a3d7672601bfa669bd2adb2003803ea8d3ec75f
santiagocolombi <santiagocolombi@hotmail.com>
2025-11-06 22:52:26 -0300

    tentando colocar dados de forma real no banco

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(public prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    await this.prisma.users.create({ data: createUserDto });
+    return createUserDto;
   }
 }
